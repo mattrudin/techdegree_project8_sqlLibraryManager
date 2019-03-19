c99b7a3501531b9dc23aecb8544ebb4939e1be6d
Matt Rudin <matthias.rudin@live.com>
2019-03-19 07:23:27 +0100

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,47 @@ const router = express.Router();
 /************************************************************************************
 Routes
 ************************************************************************************/
-// Start page
+// Home route should redirect to the /books route.
 router.get('/', (req, res) => {
-    res.render('index', {
-        title: "Books"
-    });
+    res.redirect('/books');
 });
+
+// Shows the full list of books.
+router.get('/books', (req, res) => {
+    res.render(
+
+    );
+})
+// Shows the create new book form.
+router.get('/books/new', (req, res) => {
+    res.render(
+
+    );
+})
+// Posts a new book to the database.
+router.post('/books/new', (req, res) => {
+    res.render(
+
+    );
+})
+// Shows book detail form.
+router.get('/books/:id', (req, res) => {
+    res.render(
+
+    );
+})
+// Updates book info in the database.
+router.post('/books/:id', (req, res) => {
+    res.render(
+
+    );
+})
+// Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting.
+router.post('/books/:id/delete', (req, res) => {
+    res.render(
+
+    );
+})
 
 
 module.exports = router;
