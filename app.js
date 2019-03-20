8a2847475427e18c06187dd47fc9631a9d8a2089
Matt Rudin <matthias.rudin@live.com>
2019-03-20 06:50:14 +0100

    Add routes with render and 404 page

---
+++ app.js
@@ -0,0 +1,48 @@
+/************************************************************************************
+Require functions
+************************************************************************************/
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+/************************************************************************************
+Configuration
+set and use functions
+************************************************************************************/
+//Uses the public and images folder for static files
+app.use('/static', express.static('public'));
+
+//Sets the view engine to pug
+app.set('view engine', 'pug');
+
+/************************************************************************************
+Implementing routes (located in ./routes)
+************************************************************************************/
+app.use(routes);
+
+
+/************************************************************************************
+Error handling
+************************************************************************************/
+//If route does not exist, generate a 404 error
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found');
+    err.status = 404;
+    console.log(`${err}: ${err.status}`);
+    next(err);
+});
+
+//EXTRA CREDIT: Handle errors with pug template
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status)
+    res.render('page-not-found');
+})
+
+
+/************************************************************************************
+Server setup
+************************************************************************************/
+const server = app.listen(3000, () => {
+    console.log(`The application is running on localhost:${server.address().port}`)
+});
--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,23 @@ Routes
 ************************************************************************************/
 // Home route should redirect to the /books route.
 router.get('/', (req, res) => {
-    res.redirect('/books');
+    return res.redirect('/books');
 });
 
 // Shows the full list of books.
 router.get('/books', (req, res) => {
-    res.render(
-
-    );
+    res.render('index', {title: "Books"});
 })
 // Shows the create new book form.
 router.get('/books/new', (req, res) => {
     res.render(
-
+        'new-book'
     );
 })
 // Posts a new book to the database.
 router.post('/books/new', (req, res) => {
     res.render(
-
+        'new-book'
     );
 })
 // Shows book detail form.
